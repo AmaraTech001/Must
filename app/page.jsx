3399d94dd0d23427ef58bdb90d71e0b892daefe9
Amarasystems <amarasystem.tech@gmail.com>
2025-04-15 02:29:07 +0800

    add fav icon

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+"use client";
+import { useState } from "react";
 
 export default function page() {
+  const [input, setInput] = useState(false);
+
   return (
     <div className="">
       <div className="flex justify-between border-b-[1px] p-2 border-[#dee2e6]">
         <div className="flex gap-2 px-12">
           <div>
             <a
-              className="text-[#484848] hover:underline-[1px] cursor-pointer font-bold text-[11px]"
+              className="text-[#484848] font-bold text-[11px] cursor-pointer border-b-[1px] border-transparent hover:border-[#484848] transition-all"
               href=""
             >
               МЭДЭЭ
@@ -15,7 +18,7 @@ export default function page() {
           </div>
           <div>
             <a
-              className="text-[#484848] cursor-pointer font-bold text-[11px]"
+              className="text-[#484848] cursor-pointer font-bold text-[11px] border-b-[1px] border-transparent hover:border-[#484848] transition-all"
               href=""
             >
               ЗАРЛАЛ
@@ -23,7 +26,7 @@ export default function page() {
           </div>
           <div>
             <a
-              className="text-[#484848] cursor-pointer font-bold text-[11px]"
+              className="text-[#484848] cursor-pointer font-bold text-[11px] border-b-[1px] border-transparent hover:border-[#484848] transition-all"
               href=""
             >
               SCIENCE FM 94.1
@@ -33,7 +36,7 @@ export default function page() {
         <div className="flex gap-2">
           <div>
             <a
-              className="text-[#484848] cursor-pointer font-bold text-[11px]"
+              className="text-[#484848] cursor-pointer font-bold text-[11px] border-b-[1px] border-transparent hover:border-[#484848] transition-all"
               href=""
             >
               ЭЛСЭГЧ
@@ -41,7 +44,7 @@ export default function page() {
           </div>
           <div>
             <a
-              className="text-[#484848] cursor-pointer font-bold text-[11px]"
+              className="text-[#484848] cursor-pointer font-bold text-[11px] border-b-[1px] border-transparent hover:border-[#484848] transition-all"
               href=""
             >
               ХОЛБОО БАРИХ
@@ -49,7 +52,7 @@ export default function page() {
           </div>
           <div>
             <a
-              className="text-[#484848] cursor-pointer font-bold text-[11px]"
+              className="text-[#484848] cursor-pointer font-bold text-[11px] border-b-[1px] border-transparent hover:border-[#484848] transition-all"
               href=""
             >
               АЖЛЫН БАЙР
@@ -65,6 +68,55 @@ export default function page() {
         />
         <div className="bg-[#ffc20d] max-sm:justify-end max-sm:mt-8 text-[12px] rounded-[0.3rem] font-bold p-2 hover:text-white transition-colors duration-300">
           ЧУХАЛ ХОЛБООС
+        </div>
+      </div>
+      <div className="flex px-4">
+        <div className="bg-[#f3f3f3] p-2">
+          <form className="flex gap-2 flex-wrap mb-4">
+            <div className="flex gap-1">
+              <input
+                type="radio"
+                name="searchType"
+                id="diploma"
+                value="Дипломын дугаар"
+              />
+              <label htmlFor="diploma" className="text-[14px] text-[#212529]">
+                Дипломын дугаар
+              </label>
+            </div>
+            <div className="flex gap-1">
+              <input
+                type="radio"
+                name="searchType"
+                id="register"
+                value="Регистрийн дугаар"
+              />
+              <label htmlFor="register" className="text-[14px] text-[#212529]">
+                Регистрийн дугаар
+              </label>
+            </div>
+          </form>
+          <div className="">
+            <input
+              type="text"
+              className="border-[1px] p-1 max-w-[350px] bg-white w-full border-[#ced4da]"
+            />
+          </div>
+          {input && (
+            <input
+              type="text"
+              className="border-[1px] p-1 max-w-[350px] bg-white w-full border-[#ced4da]"
+            />
+          )}
+
+          <p className="text-[14px] text-[#212529]">
+            Хэрэв таны хайсан мэдээлэл гарч ирэхгүй бол <br />
+            <span className="text-[#007bff]"> энд</span> дарж шалгана уу!
+          </p>
+
+          <div className="border-1 my-4 py-[6px] px-[12px] inline-block font-bold text-[#07158f] border-[#6c757d] rounded-[.25rem]">
+            Хайх
+          </div>
         </div>
       </div>
     </div>
