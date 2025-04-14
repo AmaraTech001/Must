8b4e9d915f73bb06ebb46b020a3264fb88611210
Amarasystems <amarasystem.tech@gmail.com>
2025-04-15 04:34:00 +0800

    a

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/mn/alumni1",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/mn/alumni1",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
